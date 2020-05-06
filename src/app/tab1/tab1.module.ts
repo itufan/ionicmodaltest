4a945ef65b1eab572ff04a709e362751658376de
itufan.de@gmail.com <it@itc.fritz.box>
2020-05-06 09:57:28 +0200

    init

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,6 +6,7 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { ModalPagePageModule } from '../modal-page/modal-page.module';
 
 @NgModule({
   imports: [
@@ -13,7 +14,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    ModalPagePageModule,
   ],
   declarations: [Tab1Page]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ModalController, IonRouterOutlet, AnimationController } from '@ionic/angular';
+import { ModalPagePage } from '../modal-page/modal-page.page';
+
+import { Plugins, KeyboardInfo } from "@capacitor/core";
+const { Keyboard } = Plugins;
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +12,68 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    private modalCtrl: ModalController,
+    private routerOutLet: IonRouterOutlet,
+    private animationCtrl: AnimationController,
+    ) {}
 
+  async presentModal() {
+
+    const enterAnimation = (baseEl: any) => {
+      const backdropAnimation = this.animationCtrl
+        .create()
+        .addElement(baseEl.querySelector("ion-backdrop")!)
+        .fromTo("opacity", "0.01", "0.33");
+
+      const wrapperAnimation = this.animationCtrl
+        .create()
+        .addElement(baseEl.querySelector(".modal-wrapper")!)
+        .fromTo("transform", "translateY(100vh)", "translateY(50vh)");
+
+      return this.animationCtrl
+        .create()
+        .addElement(baseEl)
+        .easing("cubic-bezier(0.32,0.72,0,1)")
+        .duration(320)
+        .onFinish(() => {
+          baseEl.querySelector("ion-input").setFocus();
+        })
+        .addAnimation([backdropAnimation, wrapperAnimation]);
+    };
+
+    const leaveAnimation = (baseEl: any) => {
+      const backdropAnimation = this.animationCtrl
+        .create()
+        .addElement(baseEl.querySelector("ion-backdrop")!)
+        .fromTo("opacity", "0.33", "0.0");
+
+      const wrapperAnimation = this.animationCtrl
+        .create()
+        .addElement(baseEl.querySelector(".modal-wrapper")!)
+        .fromTo("transform", "translateY(50vh)", "translateY(100vh)");
+
+      return this.animationCtrl
+        .create()
+        .addElement(baseEl)
+        .easing("cubic-bezier(0.32,0.72,0,1)")
+        .duration(320)
+        .beforeAddWrite(() => {
+          Keyboard.hide();
+        })
+        .addAnimation([backdropAnimation, wrapperAnimation]);
+    };
+
+
+
+    const modal = await this.modalCtrl.create({
+      component: ModalPagePage,
+      swipeToClose: true,
+      cssClass: "css-modal",
+      enterAnimation,
+      leaveAnimation,
+      presentingElement: this.routerOutLet.parentOutlet.nativeEl,
+    });
+    return await modal.present();
+  }
 }
